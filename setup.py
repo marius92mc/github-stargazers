acbc43064b53f23e4091310aeaaa64359f8d2ba4
marius92mc <marius92mc@gmail.com>
2017-10-12 04:48:50 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# pylint: disable=no-name-in-module,import-error
+from distutils.core import setup
+
+
+setup(
+    name='github-stargazers',
+    packages=['github-stargazers'],
+    version='0.0.1',
+    description='List stargazers and check if a user starred that repository',
+    author='Marius-Constantin Melemciuc',
+    author_email='mariuspypi@gmail.com',
+    url='https://github.com/marius92mc/github-stargazers',
+    download_url='https://github.com/' +
+    'marius92mc/github-stargazers/archive/0.0.1.tar.gz',
+    keywords=[
+        'command-line',
+        'python',
+        'github',
+        'star',
+        'repository',
+        'beautifulsoup'],
+    classifiers=[],
+)
