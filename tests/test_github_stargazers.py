3e8377f1881c00b107ea47fa5a2e9a24b4c195e1
Marius-Constantin Melemciuc <marius92mc@users.noreply.github.com>
2017-10-14 20:49:26 +0300

    Add more unit-tests (#7)
    And dropped `-s -x` flags for pytest in travis config file.

--- tests/test_github_stargazers.py
+++ tests/test_github_stargazers.py
@@ -14,45 +14,106 @@ def test_wrong_argument_raises() -> None:
 
 
 @pytest.fixture
-def url_page_content() -> str:
+def url_page_content_1() -> str:
     return "<h3>foo</h3> <h3>bar</h3>"
 
 
+@pytest.fixture
+def url_page_content_2() -> str:
+    return "<h3>foo2</h3> <h3>bar2</h3>"
+
+
 @responses.activate
-def test_get_all_stargazers_has_all_stargazers(url_page_content: str) -> None:
+def test_get_all_stargazers_has_all_stargazers(url_page_content_1: str) -> None:
     responses.add(
         responses.GET,
         "https://github.com/foo/bar/stargazers?page=1",
-        body=url_page_content,
+        body=url_page_content_1,
         status=200)
     assert set(GitHub("foo/bar").get_all_stargazers()) == set(['foo', 'bar'])
 
 
 @responses.activate
-def test_get_all_stargazers_sorts_stargazers(url_page_content: str) -> None:
+def test_get_all_stargazers_sorts_stargazers(url_page_content_1: str) -> None:
     responses.add(
         responses.GET,
         "https://github.com/foo/bar/stargazers?page=1",
-        body=url_page_content,
+        body=url_page_content_1,
         status=200)
     assert GitHub("foo/bar").get_all_stargazers() == sorted(['foo', 'bar'])
 
 
+@pytest.fixture
+def url_page_content_without_stargazers() -> str:
+    return "<html> <h1> title </h1> </html>"
+
+
 @responses.activate
-def test_provided_user_is_stargazer(url_page_content: str) -> None:
+def test_get_all_stargazers_returns_empty_on_page_without_stargazers(url_page_content_without_stargazers: str) -> None:
     responses.add(
         responses.GET,
         "https://github.com/foo/bar/stargazers?page=1",
-        body=url_page_content,
+        body=url_page_content_without_stargazers,
+        status=200)
+    assert GitHub("foo/bar").get_all_stargazers() == []
+
+
+@responses.activate
+def test_get_all_stargazers_sorts_stargazers_two_pages(url_page_content_1: str,
+                                                       url_page_content_2: str) -> None:
+    responses.add(
+        responses.GET,
+        "https://github.com/foo/bar/stargazers?page=1",
+        body=url_page_content_1,
+        status=200)
+    responses.add(
+        responses.GET,
+        "https://github.com/foo/bar/stargazers?page=2",
+        body=url_page_content_2,
+        status=200)
+    assert GitHub("foo/bar").get_all_stargazers() == sorted(['foo', 'bar', 'foo2', 'bar2'])
+
+
+@responses.activate
+def test_provided_user_is_stargazer(url_page_content_1: str) -> None:
+    responses.add(
+        responses.GET,
+        "https://github.com/foo/bar/stargazers?page=1",
+        body=url_page_content_1,
         status=200)
     assert GitHub("foo/bar").is_stargazer("foo")
 
 
 @responses.activate
-def test_provided_user_is_not_stargazer(url_page_content: str) -> None:
+def test_provided_user_is_stargazer_on_last_page(url_page_content_1: str,
+                                                 url_page_content_2: str) -> None:
     responses.add(
         responses.GET,
         "https://github.com/foo/bar/stargazers?page=1",
-        body=url_page_content,
+        body=url_page_content_1,
+        status=200)
+    responses.add(
+        responses.GET,
+        "https://github.com/foo/bar/stargazers?page=2",
+        body=url_page_content_2,
+        status=200)
+    assert GitHub("foo/bar").is_stargazer("bar2")
+
+
+@responses.activate
+def test_provided_user_is_not_stargazer(url_page_content_1: str) -> None:
+    responses.add(
+        responses.GET,
+        "https://github.com/foo/bar/stargazers?page=1",
+        body=url_page_content_1,
+        status=200)
+    assert not GitHub("foo/bar").is_stargazer("another_foo")
+
+@responses.activate
+def test_provided_user_is_not_stargazer_on_page_without_stargazers(url_page_content_without_stargazers: str) -> None:
+    responses.add(
+        responses.GET,
+        "https://github.com/foo/bar/stargazers?page=1",
+        body=url_page_content_without_stargazers,
         status=200)
     assert not GitHub("foo/bar").is_stargazer("another_foo")
