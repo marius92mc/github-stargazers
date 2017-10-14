e80114c1049318e43b7fbde05c8370de3adf9eb8
Marius-Constantin Melemciuc <marius92mc@users.noreply.github.com>
2017-10-14 21:12:25 +0300

    Add `long_description` field in setup.py (#8)
    And add pypandoc package in Pipfile.

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 # pylint: disable=no-name-in-module,import-error
+from os import path
 from setuptools import setup
+
+
+def get_long_description():
+    try:
+        import pypandoc
+        long_description = pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        long_description = open('README.md').read()
+    return long_description
 
 
 setup(
@@ -8,6 +18,7 @@ setup(
     packages=['github_stargazers'],
     version='0.0.3',
     description='List stargazers and check if a user starred that repository',
+    long_description=get_long_description(),
     author='Marius-Constantin Melemciuc',
     author_email='mariuspypi@gmail.com',
     url='https://github.com/marius92mc/github-stargazers',
