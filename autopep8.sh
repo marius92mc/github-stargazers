48760a2c04194fded3f230ce6daccc86aac9f45e
Marius-Constantin Melemciuc <marius92mc@users.noreply.github.com>
2017-10-16 16:33:06 +0300

    Add autopep8 max-line-length in setup.cfg (#21)
    Better styling for autopep.sh.
    Drop second `--aggressive` for autopep8.

--- autopep8.sh
+++ autopep8.sh
@@ -1,6 +1,10 @@
 touched_python_files=`git diff --name-only |egrep '\.py$' || true`
 
 if [ -n "$touched_python_files" ]; then
-    pipenv run autopep8 --verbose --in-place --aggressive --aggressive $touched_python_files 
+    pipenv run autopep8 \
+        --verbose \
+        --in-place \
+        --aggressive \
+        $touched_python_files
 fi 
 
